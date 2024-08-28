1208bf324304497de04d07333029a06f5bf7d18d
Aman Bhondge <amanbhondge@gmail.com>
2024-08-28 12:24:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function changeToGrey(){
+    document.getElementById("bgColor").style.backgroundColor = "grey";
+    document.getElementById("heading2").textContent = "Background Color : Grey";
+}
+function changeToGreen(){
+    document.getElementById("bgColor").style.backgroundColor = "greenyellow";
+    document.getElementById("heading2").textContent = "Background Color : Greenyellow";
+}
+function changeToBlue(){
+    document.getElementById("bgColor").style.backgroundColor = "blue";
+    document.getElementById("heading2").textContent = "Background Color : Blue";
+}
+function changeToPurple(){
+    document.getElementById("bgColor").style.backgroundColor = "purple";
+    document.getElementById("heading2").textContent = "Background Color : Purple";
+}
